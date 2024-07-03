2125bd99b68f2f84f7f4bdb6ee20b3a52d07fcb5
Tom Hennessy <tomlhennessy@outlook.com>
2024-07-03 14:04:00 +0100

    constructed dynamic array class

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,21 +1,46 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
-    // Your code here
-
+    this.data = new Array(defaultSize); // Initialise array with the given size
+    this.length = 0; // start with empty array
+    this.capacity = defaultSize; // set the initial capacity
   }
 
   read(index) {
-
-    // Your code here
+    if (index >= this.length) {
+      return undefined; // out of bounds
+    }
+    return this.data[index]; // return the value at the given index
   }
 
   unshift(val) {
+    // if the array is at capacity, double its size
+    if (this.length === this.capacity) {
+      this.resize();
+    }
 
-    // Your code here
+    // shift all elements to the right
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+
+    // insert the new value at the front
+    this.data[0] = val;
+    this.length++;
   }
 
+  // helper method to resize the array
+  resize() {
+    this.capacity *= 2; // double the capacity
+    let newData = new Array(this.capacity);
+
+    // copy old data to new data array
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+
+    this.data = newData;
+  }
 }
 
 
